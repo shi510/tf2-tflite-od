e39db27229a9937be3c60afb683008aa8b4dae26
Im Sunghoon <shi510.kor@gmail.com>
2021-07-02 00:58:01 +0900

    Rename utils unit test file

--- object_detection/unit_test/main.py
+++ object_detection/unit_test/main.py
@@ -2,7 +2,7 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import unittest
 
-from object_detection.unit_test.utils import *
+from object_detection.unit_test.utils_test import *
 
 if __name__ == '__main__':
     unittest.main()
