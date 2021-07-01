fd207db1964cc28f661d72e3b2c8c6b898e55d14
Im Sunghoon <shi510.kor@gmail.com>
2021-07-02 00:56:58 +0900

    Add grid bounding box utility

--- object_detection/lib/box_coders/utils.py
+++ object_detection/lib/box_coders/utils.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 
-def get_best_anchor_idx(boxes, anchors):
+def argmax_bbox_anchor_iou(boxes, anchors):
     """Calculate best matched anchor index.
 
     Args:
@@ -18,5 +18,46 @@ def get_best_anchor_idx(boxes, anchors):
     intersection = tf.minimum(box_wh[..., 0], anchors[..., 0]) * \
             tf.minimum(box_wh[..., 1], anchors[..., 1])
     iou = intersection / (box_area + anchor_area - intersection)
-    anchor_idx = tf.argmax(iou, axis=-1)
-    return anchor_idx
+    return tf.argmax(iou, axis=-1, output_type=tf.int32)
+
+@tf.function
+def transform_bbox_to_grided_bbox(boxes, anchors, grid_wh):
+    """Transform the bounding box into grid world.
+
+    Args:
+        boxes: (batch_size, num_boxes, (x_min, y_min, x_max, y_max, cls))
+        anchors: (num_anchors, (w, h))
+        grid_wh: [width, height]
+
+    Returns:
+        a tensor representing N anchor-encoded boxes of the format
+        (batch_size, grid_y, grid_x, num_anchors, ((x_min, y_min, x_max, y_max, obj, cls))
+    """
+    best_anchor_indices = argmax_bbox_anchor_iou(boxes, anchors) # (batch_size, num_boxes)
+    best_anchor_indices = tf.expand_dims(best_anchor_indices, -1)
+    grid_wh = tf.cast(grid_wh, dtype=tf.int32)
+    batch_size = tf.shape(boxes)[0]
+    num_boxes = tf.shape(boxes)[1]
+    num_anchors = tf.shape(anchors)[0]
+    grided_bbox = tf.zeros((batch_size, grid_wh[1], grid_wh[0], num_anchors, 6), dtype=tf.float32)
+
+    boxes_xy = (boxes[..., 0:2] + boxes[..., 2:4]) / 2
+    grid_wh = tf.cast(grid_wh, tf.float32)
+    grid_xy = tf.cast(boxes_xy // (1 / grid_wh), tf.int32) # (batch_size, num_boxes, (x, y))
+
+    indices = tf.TensorArray(tf.int32, 1, dynamic_size=True)
+    updates = tf.TensorArray(tf.float32, 1, dynamic_size=True)
+    for i in tf.range(batch_size):
+        for j in tf.range(num_boxes):
+            idx = i * num_boxes + j
+            box = boxes[i][j][0:4]
+            class_num = boxes[i][j][4]
+            anchor_idx = best_anchor_indices[i][j][0]
+            indices = indices.write(
+                idx, [i, grid_xy[i][j][1], grid_xy[i][j][0], anchor_idx])
+            updates = updates.write(
+                idx, [box[0], box[1], box[2], box[3], 1, class_num])
+
+    grided_bbox = tf.tensor_scatter_nd_update(
+        grided_bbox, indices.stack(), updates.stack())
+    return grided_bbox
--- object_detection/unit_test/utils.py
+++ object_detection/unit_test/utils.py
@@ -1,6 +1,7 @@
 import unittest
 
-from object_detection.lib.box_coders.utils import get_best_anchor_idx
+from object_detection.lib.box_coders.utils import argmax_bbox_anchor_iou
+from object_detection.lib.box_coders.utils import transform_bbox_to_grided_bbox
 
 import tensorflow as tf
 
@@ -16,8 +17,97 @@ class TestAnchorMatcher(unittest.TestCase):
             ],
             shape=(1, 3, 5),
             dtype=tf.float32)
-        
-        best_anchor = get_best_anchor_idx(boxes, anchors)
+        best_anchor = argmax_bbox_anchor_iou(boxes, anchors)
         self.assertEqual(best_anchor[0][0], 2)
         self.assertEqual(best_anchor[0][1], 0)
         self.assertEqual(best_anchor[0][2], 1)
+
+class TestTransformBoxToGridedBox(unittest.TestCase):
+
+    def test_equal_WH(self):
+        anchors = tf.constant([[128, 64], [64, 32], [32, 16]], shape=(3, 2), dtype=tf.float32)
+        boxes = tf.constant(
+            [
+                [0, 0, 30, 20, 1],
+                [15, 12, 15+130, 12+60, 2],
+                [103, 143, 103+70, 143+40, 3]
+            ],
+            shape=(1, 3, 5),
+            dtype=tf.float32)
+        img_wh = tf.constant([224, 224], dtype=tf.float32)
+        grid_wh = img_wh // 8
+        anchors /= img_wh
+        boxes_x1y1 = boxes[..., 0:2] / img_wh
+        boxes_x2y2 = boxes[..., 2:4] / img_wh
+        boxes_cls = tf.expand_dims(boxes[..., 4], -1)
+        boxes = tf.concat([boxes_x1y1, boxes_x2y2, boxes_cls], -1)
+        grided_boxes = transform_bbox_to_grided_bbox(boxes, anchors, grid_wh)
+        boxes_xy = (boxes[..., 0:2] + boxes[..., 2:4]) / 2
+        grid_xy = tf.cast(boxes_xy // (1/grid_wh), tf.int32)
+
+        def _get_grid_item(batch_id, box_id):
+            x = grid_xy[batch_id][box_id][0]
+            y = grid_xy[batch_id][box_id][1]
+            return grided_boxes[batch_id][y][x]
+
+        self.assertEqual(tf.reduce_sum(_get_grid_item(0, 0)[0], -1).numpy(), 0)
+        self.assertEqual(tf.reduce_sum(_get_grid_item(0, 0)[1], -1).numpy(), 0)
+        self.assertGreater(tf.reduce_sum(_get_grid_item(0, 0)[2], -1).numpy(), 0)
+        self.assertEqual(_get_grid_item(0, 0)[2].numpy()[4], 1) # objectness
+        self.assertEqual(_get_grid_item(0, 0)[2].numpy()[5], 1) # class id
+
+        self.assertGreater(tf.reduce_sum(_get_grid_item(0, 1)[0], -1).numpy(), 0)
+        self.assertEqual(tf.reduce_sum(_get_grid_item(0, 1)[1], -1).numpy(), 0)
+        self.assertEqual(tf.reduce_sum(_get_grid_item(0, 1)[2], -1).numpy(), 0)
+        self.assertEqual(_get_grid_item(0, 1)[0].numpy()[4], 1) # objectness
+        self.assertEqual(_get_grid_item(0, 1)[0].numpy()[5], 2) # class id
+
+        self.assertEqual(tf.reduce_sum(_get_grid_item(0, 2)[0], -1).numpy(), 0)
+        self.assertGreater(tf.reduce_sum(_get_grid_item(0, 2)[1], -1).numpy(), 0)
+        self.assertEqual(tf.reduce_sum(_get_grid_item(0, 2)[2], -1).numpy(), 0)
+        self.assertEqual(_get_grid_item(0, 2)[1].numpy()[4], 1) # objectness
+        self.assertEqual(_get_grid_item(0, 2)[1].numpy()[5], 3) # class id
+
+    def test_not_equal_WH(self):
+        anchors = tf.constant([[128, 64], [64, 32], [32, 16]], shape=(3, 2), dtype=tf.float32)
+        boxes = tf.constant(
+            [
+                [0, 0, 30, 20, 1],
+                [15, 12, 15+130, 12+60, 2],
+                [103, 143, 103+70, 143+40, 3]
+            ],
+            shape=(1, 3, 5),
+            dtype=tf.float32)
+        img_wh = tf.constant([448, 224], dtype=tf.float32)
+        grid_wh = img_wh // 8
+        anchors /= img_wh
+        boxes_x1y1 = boxes[..., 0:2] / img_wh
+        boxes_x2y2 = boxes[..., 2:4] / img_wh
+        boxes_cls = tf.expand_dims(boxes[..., 4], -1)
+        boxes = tf.concat([boxes_x1y1, boxes_x2y2, boxes_cls], -1)
+        grided_boxes = transform_bbox_to_grided_bbox(boxes, anchors, grid_wh)
+        boxes_xy = (boxes[..., 0:2] + boxes[..., 2:4]) / 2
+        grid_xy = tf.cast(boxes_xy // (1/grid_wh), tf.int32)
+
+        def _get_grid_item(batch_id, box_id):
+            x = grid_xy[batch_id][box_id][0]
+            y = grid_xy[batch_id][box_id][1]
+            return grided_boxes[batch_id][y][x]
+
+        self.assertEqual(tf.reduce_sum(_get_grid_item(0, 0)[0], -1).numpy(), 0)
+        self.assertEqual(tf.reduce_sum(_get_grid_item(0, 0)[1], -1).numpy(), 0)
+        self.assertGreater(tf.reduce_sum(_get_grid_item(0, 0)[2], -1).numpy(), 0)
+        self.assertEqual(_get_grid_item(0, 0)[2].numpy()[4], 1) # objectness
+        self.assertEqual(_get_grid_item(0, 0)[2].numpy()[5], 1) # class id
+
+        self.assertGreater(tf.reduce_sum(_get_grid_item(0, 1)[0], -1).numpy(), 0)
+        self.assertEqual(tf.reduce_sum(_get_grid_item(0, 1)[1], -1).numpy(), 0)
+        self.assertEqual(tf.reduce_sum(_get_grid_item(0, 1)[2], -1).numpy(), 0)
+        self.assertEqual(_get_grid_item(0, 1)[0].numpy()[4], 1) # objectness
+        self.assertEqual(_get_grid_item(0, 1)[0].numpy()[5], 2) # class id
+
+        self.assertEqual(tf.reduce_sum(_get_grid_item(0, 2)[0], -1).numpy(), 0)
+        self.assertGreater(tf.reduce_sum(_get_grid_item(0, 2)[1], -1).numpy(), 0)
+        self.assertEqual(tf.reduce_sum(_get_grid_item(0, 2)[2], -1).numpy(), 0)
+        self.assertEqual(_get_grid_item(0, 2)[1].numpy()[4], 1) # objectness
+        self.assertEqual(_get_grid_item(0, 2)[1].numpy()[5], 3) # class id
